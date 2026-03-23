312f2a7d36a44de728ad42ffbac24a53fb414fc8
Vidhisha Pattipaka <146458746+vidhishapattipaka@users.noreply.github.com>
2026-03-23 11:27:01 +0530

    Update ReviewCard.jsx

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -11,7 +11,38 @@ function ReviewCard({ review }) {
           <span className="review-role">Tea Lover</span>
         </div>
       </div>
+function ReviewCard({ review }) {
+  return (
+    <div className="review-card-elegant">
+      
+      {/* Quote Icon */}
+      <span className="review-quote">❝</span>
 
+      <div className="review-top">
+        <div className="review-avatar">
+          {review.name.charAt(0)}
+        </div>
+
+        <div>
+          <h4>{review.name}</h4>
+          <span className="review-role">Tea Lover</span>
+        </div>
+      </div>
+
+      {/* Stars */}
+      <div className="review-stars">
+        {'★'.repeat(5)}
+      </div>
+
+      {/* Review Text */}
+      <p className="review-text">
+        “{review.text}”
+      </p>
+    </div>
+  )
+}
+
+export default ReviewCard
       <div className="review-stars">★★★★★</div>
 
       <p className="review-text">“{review.text}”</p>
