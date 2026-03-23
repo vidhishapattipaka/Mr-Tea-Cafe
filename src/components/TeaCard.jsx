943fb3cc549d826fca6d89c98c65f0dabb8d6a73
Vidhisha Pattipaka <146458746+vidhishapattipaka@users.noreply.github.com>
2026-03-23 11:27:36 +0530

    Update TeaCard.jsx

--- src/components/TeaCard.jsx
+++ src/components/TeaCard.jsx
@@ -1,12 +1,22 @@
 function TeaCard({ item }) {
   return (
     <div className="card tea-card">
-      {item.image && <img src={item.image} alt={item.name} className="card-image" />}
+
+      {/* Image */}
+      {item.image && (
+        <div className="tea-image-wrapper">
+          <img src={item.image} alt={item.name} className="card-image" />
+          <span className="tea-badge">Popular</span>
+        </div>
+      )}
+
+      {/* Content */}
       <div className="card-body">
         <div className="card-top">
           <h3>{item.name}</h3>
-          <span>{item.price}</span>
+          <span className="price">{item.price}</span>
         </div>
+
         {item.description && <p>{item.description}</p>}
       </div>
     </div>
