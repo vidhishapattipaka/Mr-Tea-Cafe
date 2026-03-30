193edf9ac32718b44113c8c48c6db586fa6a2191
Vidhisha Pattipaka <vidhishapattipaka03@gmail.com>
2026-03-30 16:27:08 +0530

    fix base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mr-Tea-Cafe/',
+  base: process.env.NODE_ENV === 'production'
+    ? '/Mr-Tea-Cafe/'
+    : '/',
 })
